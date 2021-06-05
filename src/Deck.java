e942e07ff4fe95bd041e03c714866be3a17fc7e0
crab-a <crab@notmailinator.com>
2021-06-05 10:40:56 +0300

    deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,29 @@
-public class Deck {
+import java.util.*;
 
+public class Deck {
+    public List<Card> deck;
+    private int lastCardIndex;
+    public Deck(boolean makeDeck){
+        if(makeDeck) {
+            for (Shape shape : Shape.values()) {
+                for (Value value : Value.values()) {
+                    addCard(new Card(value, shape));//change to add card
+                }
+            }
+            this.lastCardIndex=this.deck.size()-1;
+        }
+    }
+    void addCard(Card card){ deck.add(card); }
+
+    Card removeTopCard(){
+        Card topCard = this.deck.get(deck.size()-1);
+        this.deck.remove(this.lastCardIndex--);
+        return  topCard;
+    }
+
+    boolean isEmpty(){ return this.lastCardIndex!=0; }
+
+    void shuffle(){
+
+    }
 }
--- src/Value.java
+++ src/Value.java
@@ -1,7 +1,7 @@
 public enum Value {
-    N2(2,"2"), N3(3,"3"), N4(4,"4"), N5(5,"5"), N6(6,"6"),
+    ACE(1,"Ace"), N2(2,"2"), N3(3,"3"), N4(4,"4"), N5(5,"5"), N6(6,"6"),
     N7(7,"7"), N8(8,"8"), N9(9,"9"), N10(10,"10"),
-    JACK(11,"Jack"), QUEEN(12,"Queen"), KING(13,"King"), ACE(1,"Ace");
+    JACK(11,"Jack"), QUEEN(12,"Queen"), KING(13,"King");
     Value(int value, String name) {
         this.value = value;
         this.name=name;
