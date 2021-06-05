c8be9198e018a089eb24d06a77b84ac67e0b7612
crab-a <crab@notmailinator.com>
2021-06-05 15:48:54 +0300

    we need to hide the draw so it will be printed like so
    Or won the war
    +++++++++++++++++++++++++++++++++++++++
    Or won the game!
    
    and NOT like (current)
    -------------------------155-------------------------
    Or drew 8 of ♦
    +++++++++++++++++++++++++++++++++++++++
    Or won the game!

--- src/Deck.java
+++ src/Deck.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 public class Deck {
     public List<Card> deck;
-    private int lastCardIndex;
+    private int lastCardIndex=0;
 
     public Deck(boolean makeDeck) {
+        this.deck=new ArrayList<Card>();
         if (makeDeck) {
             for (Shape shape : Shape.values()) {
                 for (Value value : Value.values()) {
                     addCard(new Card(value, shape));//change to add card
                 }
             }
-        } else {
-            this.lastCardIndex = -1;
         }
+        this.lastCardIndex--;
     }
     public Card getTopCard(){
         return this.deck.get(lastCardIndex);
@@ -36,7 +36,7 @@ public class Deck {
 
     void shuffle() {
         for (int i = 0; i < 50; i++) {
-            int first = Main.rnd.nextInt(), sec = Main.rnd.nextInt();
+            int first = Main.rnd.nextInt(this.lastCardIndex), sec = Main.rnd.nextInt(this.lastCardIndex);
             Card temp = this.deck.get(first);
             this.deck.set(first, this.deck.get(sec));
             this.deck.set(sec, temp);
