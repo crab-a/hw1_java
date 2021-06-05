eb1b4825e9821d05b2f81b909572648921de2234
Menachem Franko <franko@campus.technion.ac.il>
2021-06-05 11:51:51 +0300

    123

--- src/WarGame.java
+++ src/WarGame.java
@@ -4,13 +4,26 @@ public class WarGame {
     private Deck openDeckPlayer1;
     private Deck openDeckPlayer2;
     public WarGame(String nameA, String nameB){
-
         player1 = new Player(name1);
         player2 = new Player(name2);
         openDeckPlayer1 = new Deck(false);
         openDeckPlayer2 = new Deck(false);
     }
     public void initializeGame(){
-        public
+        Deck initDeck = new Deck(true);
+        initDeck.shuffle();
+        int counter = 0;
+        while (!initDeck.isEmpty()){
+            Player[] playerArr = {player1, player2};
+            playerArr[counter%2].addPlayDeck(initDeck.removeTopCard());
+            counter++;
+        }
+    }
+    public String start(){
+        initializeGame();
+
+    }
+    public void round(){
+
     }
 }
