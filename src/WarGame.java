d96ce9bbc584d835146f6c083d52df24b24e7206
Menachem Franko <franko@campus.technion.ac.il>
2021-06-05 11:36:46 +0300

    12

--- src/WarGame.java
+++ src/WarGame.java
@@ -1,5 +1,16 @@
 public class WarGame {
-    public Player player1;
-    public Player player2;
+    private Player player1;
+    private Player player2;
+    private Deck openDeckPlayer1;
+    private Deck openDeckPlayer2;
+    public WarGame(String nameA, String nameB){
 
+        player1 = new Player(name1);
+        player2 = new Player(name2);
+        openDeckPlayer1 = new Deck(false);
+        openDeckPlayer2 = new Deck(false);
+    }
+    public void initializeGame(){
+        public
+    }
 }
