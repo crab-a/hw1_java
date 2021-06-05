493ad3ca3b0d2933ece3bcc65dfa1a02ee46779d
Menachem Franko <franko@campus.technion.ac.il>
2021-06-05 10:42:49 +0300

    push

---
+++ src/Player.java
@@ -0,0 +1,20 @@
+public class Player {
+    private String name;
+    public Deck playDeck;
+    public Deck winDeck;
+    public Player(String name){
+        this.name = name;
+        this.playDeck = new Deck(false);
+        this.winDeck = new Deck(false);
+    }
+    public void addPlayDeck(Card card){
+        this.playDeck.addCard(card);
+    }
+    public void addWinDeck(Card card){
+        this.winDeck.addCard(card);
+    }
+    public Card drawCard(){
+        return this.playDeck.removeTopCard();
+    }
+
+}
